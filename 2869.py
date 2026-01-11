4a10c7c967e79c641d78fba292b1d030644cf3ec
정희재 <sanhobox@gmail.com>
2026-01-12 04:24:06 +0900

    [수학] BOJ 2869 풀이

---
+++ 2869.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+A, B, V = map(int, input().split())
+
+day_count = 1
+snail_height = 0
+snail_daily_height = A - B
+V -= A
+
+day_count += math.ceil(V / snail_daily_height)
+
+print(day_count)
